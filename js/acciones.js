35752ab22b9d3808606fc3098de961a380849d7d
ConejoCaco <huanca1992mamani@gmail.com>
2023-06-29 15:22:37 -0400

    as

--- js/acciones.js
+++ js/acciones.js
@@ -1,8 +1,9 @@
-function agregarDiv() {
+function agregarDiv(event) {
+    event.preventDefault();
     var div = document.createElement("div");
     div.classList.add("contenido");
-    div.innerHTML = "<h3>Contenido agregado dinámicamente</h3><p>Este es el contenido del div agregado al presionar el botón.</p>";
+    div.innerHTML = "<h3>Contenido agregado dinamicamente</h3><p>Este es el contenido del div agregado al presionar el botón.</p>";
     document.body.appendChild(div);
   }
-var elboton = document.getElementById("elboton");
-elboton.addEventListener("click", agregarDiv);
+
+document.getElementById("elboton").addEventListener("click", agregarDiv);
